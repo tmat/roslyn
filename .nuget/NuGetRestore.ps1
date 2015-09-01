e73f8d92ae3b6ecacfcb2847dce7cbd5ced7e27c
Tomas Matousek <tomas.matousek@microsoft.com>
2015-09-01 14:00:08 -0700

    TODO

--- .nuget/NuGetRestore.ps1
+++ .nuget/NuGetRestore.ps1
@@ -1,9 +1,8 @@
 $NuGetExe = "$PSScriptRoot\NuGet.exe"
-$NuGetV3Exe = "$PSScriptRoot\V3\NuGet.exe"
+$NuGetRoot = "$env:UserProfile\.nuget\packages"
 
-& $NuGetExe restore "$PSScriptRoot\packages.config" -PackagesDirectory "$PSScriptRoot\..\packages" -ConfigFile "$PSScriptRoot\NuGet.config"
-& $NuGetV3Exe restore "$PSScriptRoot\V3\packages.config" -PackagesDirectory "$PSScriptRoot\..\packages" -ConfigFile "$PSScriptRoot\NuGet.config"
-& $NuGetV3Exe restore "$PSScriptRoot\..\Roslyn.sln" -ConfigFile "$PSScriptRoot\NuGet.config"
-& $NuGetExe restore "$PSScriptRoot\..\src\Samples\Samples.sln" -ConfigFile "$PSScriptRoot\NuGet.config"
-& $NuGetExe restore "$PSScriptRoot\..\src\Dependencies\Dependencies.sln" -ConfigFile "$PSScriptRoot\NuGet.config"
+& $NuGetExe restore "$PSScriptRoot\packages.config" -PackagesDirectory $NuGetRoot -ConfigFile "$PSScriptRoot\NuGet.config"
+& $NuGetExe restore "$PSScriptRoot\..\Roslyn.sln" -PackagesDirectory $NuGetRoot -ConfigFile "$PSScriptRoot\NuGet.config"
+& $NuGetExe restore "$PSScriptRoot\..\src\Samples\Samples.sln" -PackagesDirectory $NuGetRoot -ConfigFile "$PSScriptRoot\NuGet.config"
+& $NuGetExe restore "$PSScriptRoot\..\src\Dependencies\Dependencies.sln" -PackagesDirectory $NuGetRoot -ConfigFile "$PSScriptRoot\NuGet.config"
 
