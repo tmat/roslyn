058f2ec4fa78066ece3549ef4b04c1bce227014d
tmat <tomas.matousek@microsoft.com>
2024-04-05 15:37:33 -0700

    External Access

---
+++ src/Tools/ExternalAccess/Copilot/SemanticSearch/SemanticSearchWorkspaceHostWrapper.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.ExternalAccess.Copilot.SemanticSearch;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.SemanticSearch;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Copilot.Internal.SemanticSearch;
+
+[Export(typeof(ICopilotSemanticSearchWorkspaceHost)), Shared]
+[method: ImportingConstructor]
+[method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+internal sealed class SemanticSearchWorkspaceHostWrapper(
+    Lazy<ISemanticSearchWorkspaceHost> host) : ICopilotSemanticSearchWorkspaceHost
+{
+    public Task<Document> UpdateQueryDocumentAsync(string query, CancellationToken cancellationToken)
+        => host.Value.Workspace.UpdateQueryDocumentAsync(query, cancellationToken);
+}
