78deafe2fcb7cd3fd3b487cb70b1764ebd0b9d9c
Tomas Matousek <tomat@microsoft.com>
2025-12-08 14:08:46 -0800

    1

---
+++ src/T/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    F();
+    Thread.Sleep(1000);
+}
+
+void F()
+{
+    Console.WriteLine("X25");
+}
